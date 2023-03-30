2509f878dbb0873df12624ffa3feac304ae62ea4
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2023-03-30 19:21:09 -0400

    updated

--- Terraform_CFD/cloudfront.tf
+++ Terraform_CFD/cloudfront.tf
@@ -1,17 +1,3 @@
-module "s3" {
-  source = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/s3"
-  bucket_name = var.cloudfront_distribution_bucket_name
-  stack_name = var.stack_name
-  env = terraform.workspace
-  tags = var.tags
-  s3_force_destroy = var.s3_force_destroy
-  days_for_archive_tiering = 125
-  days_for_deep_archive_tiering = 180
-  s3_enable_access_logging = true
-  s3_access_log_bucket_id = ""
-}
-
-
 module "cloudfront" {
   count = var.create_cloudfront ? 1 : 0
   source = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/cloudfront"
