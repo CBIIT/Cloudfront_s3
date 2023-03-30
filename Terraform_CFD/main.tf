ca996b8ea963dda0309c2c5a594b412031bf709f
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2023-03-30 15:58:41 -0400

    Update main.tf

--- Terraform_CFD/main.tf
+++ Terraform_CFD/main.tf
@@ -25,5 +25,5 @@ module "cloudfront" {
   tags = var.tags
   create_files_bucket = var.create_files_bucket
   target_account_cloudone = var.target_account_cloudone
-  public_key_path = file("${path.module}/workspace/gmb_public_key.pem")
+  public_key_path = file("${path.module}/workspace/icdc_public_key.pem")
 }
