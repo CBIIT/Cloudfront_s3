bcf7d011359b375e0e978e31c18eab2cd08f0c3e
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2023-03-31 15:10:52 -0400

    Update cloudfront.tf

--- Terraform_CFD/cloudfront.tf
+++ Terraform_CFD/cloudfront.tf
@@ -3,7 +3,7 @@ module "cloudfront" {
   source = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/cloudfront"
   alarms = var.alarms
   domain_name = var.domain_name
-  cloudfront_distribution_bucket_name = var.cloudfront_distribution_bucket_name
+  cloudfront_distribution_bucket_name = module.s3.bucket_name
   cloudfront_slack_channel_name =  var.cloudfront_slack_channel_name
   env = terraform.workspace
   stack_name = var.stack_name
