b454cdeecb5c44a147b51621d0edb8107707bf7d
Charlo237 <101688187+Charlo237@users.noreply.github.com>
2023-03-31 14:21:37 -0400

    Update variables.tf

--- Terraform_CFD/variables.tf
+++ Terraform_CFD/variables.tf
@@ -35,7 +35,7 @@ variable "create_cloudfront" {
 variable "create_files_bucket" {
   description = "indicate if you want to create files bucket or use existing one"
   type = bool
-  default = false
+  default = true
 }
 
 variable "domain_name" {
